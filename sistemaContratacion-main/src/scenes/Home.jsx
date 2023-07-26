bddfa8aad6233390434841e95ab2bfe6c838c067
Kevin Vargas <70618510+KernyMC@users.noreply.github.com>
2023-07-25 21:16:59 -0500

    Update and rename principal to Home.jsx

---
+++ sistemaContratacion-main/src/scenes/Home.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Button,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import DrawerComp from "./Drawer";
+
+const Header = () => {
+  const [value, setValue] = useState();
+  const theme = useTheme();
+  console.log(theme);
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+  console.log(isMatch);
+
+  return (
+    <React.Fragment>
+      <AppBar sx={{ background: "#006935" }}>
+        <Toolbar>
+          <espeLogo sx={{ transform: "scale(2)" }} />
+          {isMatch ? (
+            <>
+              <Typography sx={{ fontSize: "2rem", paddingLeft: "10%" }}>
+                Concurso de Meritos y Oposición 2023
+              </Typography>
+              <DrawerComp />
+            </>
+          ) : (
+            <>
+              <Tabs
+                sx={{ marginLeft: "auto" }}
+                indicatorColor="secondary"
+                textColor="inherit"
+                value={value}
+                onChange={(e, value) => setValue(value)}
+              >              </Tabs>
+              <Button
+                sx={{
+                  marginLeft: "auto",
+                  background: "#006935",
+                  "&:hover": {
+                    background: "#00a362", // Color verde más claro para el hover
+                  },
+                }}
+                variant="contained"
+              >
+                Iniciar Sesión
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
+
+export default Header;
+
