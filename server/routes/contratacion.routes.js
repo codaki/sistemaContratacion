0945b49ebade1a403b163f495aaa405a30184e27
Genesis-Calapaqui <gicalapaqui@espe.edu.ec>
2023-08-06 13:20:31 -0500

    Update contratacion.routes.js

--- server/routes/contratacion.routes.js
+++ server/routes/contratacion.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { getContratacion } from "../controllers/contratacion.controller.js";
+import { getContratacion,createContratacion,updateContratacion,deleteContratacion } from "../controllers/contratacion.controller.js";
 
 const router = Router();
 
 router.get("/contratacion",getContratacion);
+router.post("/contratacion",createContratacion);
+router.put("/contratacion/:con_id",updateContratacion);
+router.delete("/contratacion/:con_id",deleteContratacion);
+
 export default router;
