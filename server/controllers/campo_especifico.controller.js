bd3db206844b4ae261cd6c47d1f8795924e6026f
Genesis-Calapaqui <gicalapaqui@espe.edu.ec>
2023-08-06 13:19:18 -0500

    Update campo_especifico.controller.js

--- server/controllers/campo_especifico.controller.js
+++ server/controllers/campo_especifico.controller.js
@@ -7,3 +7,32 @@ export const getCampoEspecifico= (req, res) => {
         return res.status(200).json(data.rows);
     });
 };
+export const createCampoEspecifico = (req, res) => {
+    const {ce_nombre, ce_descripcion,ca_id} = req.body;
+    const q = "INSERT INTO campo_especifico (ce_nombre, ce_descripcion,ca_id) VALUES ($1,$2,$3)";
+    const values = [ce_nombre,ce_descripcion,ca_id];
+    db.query(q, values, (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json("Campo específico creado");
+    });
+}
+
+export const updateCampoEspecifico = (req,res) => {
+    const ce_id = req.params.ca_id;
+    const {ce_nombre,ce_descripcion,ca_id} = req.body;
+    const q = "UPDATE campo_especifico SET ce_nombre=$1,ce_descripcion=$2,ca_id=$3 WHERE ce_id=$4";
+    const values = [ce_nombre,ce_descripcion,ca_id,ce_id];
+    db.query(q, values, (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json("Campo específico actualizado");
+    });
+}
+export const deleteCampoEspecífico = (req,res) => {
+    const ce_id = req.params.ce_id;
+    const q = "DELETE FROM campo_especifico WHERE ce_id=$1";
+    const values = [ce_id];
+    db.query(q, values, (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json("Campo específico eliminado");
+    }); 
+}
