61ca14bc786d5739180c6b9350ef8e86dba0f339
Genesis-Calapaqui <gicalapaqui@espe.edu.ec>
2023-08-06 13:20:13 -0500

    Update actividad.routes.js

--- server/routes/actividad.routes.js
+++ server/routes/actividad.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
-import { getActividad } from "../controllers/actividad.controller.js";
+import { getActividad,createActividad,updateActividad,deleteActividad } from "../controllers/actividad.controller.js";
 
 const router = Router();
 
 router.get("/actividad",getActividad);
+router.post("/actividad",createActividad);
+router.put("/actividad/:act_id",updateActividad);
+router.delete("/actividad/:act_id",deleteActividad);
 export default router;
