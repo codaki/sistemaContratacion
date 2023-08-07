32db9144541182ef0a90f03a748eead197e732b7
Adriana Lizbeth Diaz Bautista <aldiaz5@espe.edu.ec>
2023-08-06 21:03:02 -0500

    botones y formatos en dashboard

--- sistemaContratacion-main/src/scenes/dashboard/index.jsx
+++ sistemaContratacion-main/src/scenes/dashboard/index.jsx
@@ -1,7 +1,19 @@
-import { Box, Typography, useTheme, Grid, Button, Container, } from "@mui/material";
+import { Box, Typography, useTheme, Button, Container, } from "@mui/material";
 import { tokens } from "../../theme";
 import styled from "@emotion/styled";
-import heroImg from "./portada1.png";
+import { useState } from "react";
+import { 
+  Dialog, 
+  DialogTitle, 
+  DialogContent, 
+  DialogContentText, 
+  DialogActions, 
+  Link } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import Excel from './excel.jpeg';
+import Pdf from './pdf.png';
+import Word from './word.png';
+
 const Dashboard = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
@@ -28,6 +40,43 @@ const Dashboard = () => {
     },
   }));
 
+  const [open, setOpen] = useState(false);
+
+  const [button1Color, setButton1Color] = useState('primary');
+  const [button2Color, setButton2Color] = useState('primary');
+
+  const handleButtonClick = (buttonNumber) => {
+    if (buttonNumber === 1) {
+      setButton1Color(button1Color === 'primary' ? 'secondary' : 'primary');
+    } else if (buttonNumber === 2) {
+      setButton2Color(button2Color === 'primary' ? 'secondary' : 'primary');
+    }
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const boxStyle = {
+    display: 'inline-flex',
+    flexDirection:'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '5px',
+  };
+
+  const linkStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textDecoration: 'none',
+    color: '#000',
+  };
+
   return (
     <Box sx={{ backgroundColor: "#caf7dc", minHeight: "80vh" }}>
     <Container>
@@ -50,13 +99,93 @@ const Dashboard = () => {
           </Title>
           <Typography
             variant="body2"
-            sx={{ fontSize: "18px", color: "#5A6473", my: 4 }}
+            sx={{ fontSize: "18px", color: "#5A6473", my: 2 }}
           >
           Para mayor información dale click al botón
           </Typography>
-          {/* <Button variant="contained" color="primary" sx={{ flexGrow: 1 }}>
-              Botón 1
-            </Button> */}
+          <Box alignContent={"center"}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Button 
+          variant="contained" 
+          color="primary" 
+          onClick={handleOpen}>
+            Formatos Aqui!
+          </Button>
+          </Box>
+
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle align='center'><span>FORMATOS VALIDOS</span></DialogTitle>
+            <DialogContent color="black">
+              <DialogContentText>
+                Aquí encontrarás los formatos de archivo disponibles que puedes usar para subir tu información:
+              </DialogContentText>
+  
+              <Box padding={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <Box style={boxStyle}>
+                  <Link href="https://docs.google.com/spreadsheets/d/1BI8Nsc6NLHcvn3YTrSsiDUijB6tlqvAZ/edit?usp=sharing&ouid=110804931965251293168&rtpof=true&sd=true" target="_blank" rel="noopener" style={linkStyle}>
+                    <img src={Excel} alt="formato excel hoja de vida" width={60}/>
+                    <span>HOJA DE VIDA FORMATO ESPE</span>
+                  </Link>
+                </Box>
+              </Box>
+              <Box padding={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <Box style={boxStyle}>
+                  <Link href="https://drive.google.com/drive/folders/1SVSiyfRVn_bSfRZN4n5-nl5QAhoLa0ea?usp=sharing" target="_blank" rel="noopener" style={linkStyle}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <img src={Pdf} alt="formato certificado pdf" width={60} />
+                      <img src={Word} alt="formato certificado word" width={60} />
+                    </div>
+                    <span>CERTIFICADOS EXPERIENCIA PROFESIONAL DOCENTE</span>
+                  </Link>
+                  
+                </Box>
+              </Box>
+              <Box pt={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <Box style={boxStyle}>
+                  <Link href="https://drive.google.com/drive/folders/1VPXTLAUtw2EmKRxKYUT-tPz2RxdD69Np?usp=drive_link" target="_blank" rel="noopener" style={linkStyle}>
+                   <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <img src={Pdf} alt="formato certificado pdf" width={60} />
+                      <img src={Word} alt="formato certificado word" width={60} />
+                    </div>
+                    <span>CERTIFICADOS EXPERIENCIA PROFESIONAL</span>
+                  </Link>
+                </Box>
+              </Box>
+            </DialogContent>
+            
+            <DialogActions>
+              <Button onClick={handleClose} color="secondary">
+                <CloseIcon/>
+                Cerrar
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </Box>
+        <br/>
+        <Box sx={{ display: 'flex', gap: 2 }}>
+      <Button
+        variant="contained"
+        color={button1Color}
+        onClick={() => handleButtonClick(1)}
+        sx={{ flexGrow: 1 }}
+      >
+        Estado Postulación
+      </Button>
+
+      <Button
+        variant="contained"
+        color={button2Color}
+        onClick={() => handleButtonClick(2)}
+        sx={{ flexGrow: 1 }}
+      >
+        Estado Información
+      </Button>
+
+      <br/>
+      <br/>
+    </Box>
+    <br/>
+
         </Box>
         <Box
             bgcolor="#f1f1f1"
@@ -71,16 +200,7 @@ const Dashboard = () => {
             backgroundImage: `url(https://uec-el.espe.edu.ec/wp-content/uploads/2018/11/espe-banner-estudiantes.jpg)`,
             backgroundSize: "cover",
             backgroundPosition: "-40vh 0",
-            backgroundRepeat: "no-repeat", }}>
-            {/* <Button variant="contained" color="primary" sx={{ flexGrow: 1 }}>
-              Botón 1
-            </Button>
-            <Button variant="contained" color="secondary" sx={{ flexGrow: 1 }}>
-              Botón 2
-            </Button>
-            <Button variant="contained" color="secondary" sx={{ flexGrow: 1 }}>
-              Botón 3
-            </Button> */}
+            backgroundRepeat: "no-repeat", }}>           
           </Box>
     
       </CustomBox>
