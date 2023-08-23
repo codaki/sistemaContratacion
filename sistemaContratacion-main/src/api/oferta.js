2c290a0b6e73a6b922a5cefebac434ceecbaa5bd
Christopher Iza <cdiza5@espe.edu.ec>
2023-08-22 23:59:10 -0500

    Filtro Postulacion

--- sistemaContratacion-main/src/api/oferta.js
+++ sistemaContratacion-main/src/api/oferta.js
@@ -7,6 +7,42 @@ export const pedirOferta = () =>
     withCredentials: true,
   });
 
+  export const postulacionUnica = () =>
+  axios.get(`${API}/postulacionUnica`, {
+    withCredentials: true,
+  });
+  export const contratacionUnica = (post_id)=> 
+  axios.get(`${API}/contratacionUnica/${post_id}`, {
+    withCredentials: true,
+  });
+  export const sedeUnica = (post_id,con_id)=>
+  axios.get(`${API}/sedeUnica/${post_id}/${con_id}`, {
+    withCredentials: true,
+  });
+  export const departamentoUnico = (post_id,con_id,sede_id)=>
+  axios.get(`${API}/departamentoUnico/${post_id}/${con_id}/${sede_id}`, {
+    withCredentials: true,
+  });
+  export const campoAmplioUnico = (post_id,con_id,sede_id,dept_id)=>
+  axios.get(`${API}/campoAmplioUnico/${post_id}/${con_id}/${sede_id}/${dept_id}`, {
+    withCredentials: true,
+  });
+  export const campoEspecificoUnico = (post_id,con_id,sede_id,dept_id,ca_id)=>
+  axios.get(`${API}/campoEspecificoUnico/${post_id}/${con_id}/${sede_id}/${dept_id}/${ca_id}`, {
+    withCredentials: true,
+  });
+  export const personalUnico = (post_id,con_id,sede_id,dept_id,ca_id,ce_id)=>
+  axios.get(`${API}/personalUnico/${post_id}/${con_id}/${sede_id}/${dept_id}/${ca_id}/${ce_id}`, {
+    withCredentials: true,
+  });
+  export const actividadUnica = (post_id,con_id,sede_id,dept_id,ca_id,ce_id,pa_id)=>
+  axios.get(`${API}/actividadUnica/${post_id}/${con_id}/${sede_id}/${dept_id}/${ca_id}/${ce_id}/${pa_id}`, {
+    withCredentials: true,
+  });
+  export const obtenerOferta = (post_id,con_id,sede_id,dept_id,ca_id,ce_id,pa_id,act_id)=>
+  axios.get(`${API}/obtenerOferta/${post_id}/${con_id}/${sede_id}/${dept_id}/${ca_id}/${ce_id}/${pa_id}/${act_id}`, {
+    withCredentials: true,
+  });
 export const crearOferta = async (formData) => {
   try {
     const response = await axios.post(`${API}/oferta`, formData, {
