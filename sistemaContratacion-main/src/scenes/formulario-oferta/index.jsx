3dcb521bd4587942476c7073d60baf0bcd5e1c4b
Adriana Lizbeth Diaz Bautista <aldiaz5@espe.edu.ec>
2023-08-27 14:20:59 -0500

    formulariOferta campos para escoger

--- sistemaContratacion-main/src/scenes/formulario-oferta/index.jsx
+++ sistemaContratacion-main/src/scenes/formulario-oferta/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Header from "../../components/Header";
@@ -8,15 +9,15 @@ import { crearOferta } from "../../api/oferta";
 const FormulariOferta = () => {
   const [formData, setFormData] = useState({
     post_periodo: "",
-    connombre: "",
-    cenombre: "",
-    canombre: "",
-    sedenombre: "",
-    deptnombre: "",
-    panombre: "",
-    actnombre: "",
-    ohnombre: "",
-    ovnombre: "",
+    con_nombre: "",
+    ce_nombre: "",
+    ca_nombre: "",
+    sede_nombre: "",
+    dept_nombre: "",
+    pa_nombre: "",
+    act_nombre: "",
+    ofe_vacantes: "",
+    ofe_horas: "",
   });
 
   const handleChange = (event) => {
@@ -49,86 +50,125 @@ const FormulariOferta = () => {
       >
         <Header title="Formulario Oferta" subtitle="Complete el formulario" />
         <label>Periodo Postulación:</label>
-        <TextField
+        <Select
           name="post_periodo"
           value={formData.post_periodo}
           onChange={handleChange}
-          type="number"
           margin="normal"
           required
-        />
+        >
+          <MenuItem value="periodo1">Periodo 1</MenuItem>
+          <MenuItem value="periodo2">Periodo 2</MenuItem>
+          
+        </Select>
         <label>Tipo de Contratación:</label>
-        <TextField
-          name="connombre"
-          value={formData.connombre}
+        <Select
+          name="con_nombre"
+          value={formData.con_nombre}
           onChange={handleChange}
           margin="normal"
           required
-        />
+        >
+          <MenuItem value="contratacion1">Contratación 1</MenuItem>
+          <MenuItem value="contratacion2">Contratación 2</MenuItem>
+          
+        </Select>
         <label>Campo Específico:</label>
-        <TextField
-          name="cenombre"
-          value={formData.cenombre}
+        <Select
+          name="ce_nombre"
+          value={formData.ce_nombre}
           onChange={handleChange}
           margin="normal"
           required
-        />
+        >
+          <MenuItem value="campoEspecifico1">Campo Específico 1</MenuItem>
+          <MenuItem value="campoEspecifico2">Campo Específico 2</MenuItem>
+          
+        </Select>
         <label>Campo Amplio:</label>
-        <TextField
-          name="canombre"
-          value={formData.canombre}
+        <Select
+          name="ca_nombre"
+          value={formData.ca_nombre}
           onChange={handleChange}
           margin="normal"
           required
-        />
+        >
+          <MenuItem value="campoAmplio1">Campo Amplio 1</MenuItem>
+          <MenuItem value="campoAmplio2">Campo Amplio 2</MenuItem>
+          
+        </Select>
         <label>Sede:</label>
-        <TextField
-          name="sedenombre"
-          value={formData.sedenombre}
+        <Select
+          name="sede_nombre"
+          value={formData.sede_nombre}
           onChange={handleChange}
           margin="normal"
           required
-        />
+        >
+          <MenuItem value="sede1">Sede 1</MenuItem>
+          <MenuItem value="sede2">Sede 2</MenuItem>
+          
+        </Select>
         <label>Departamento:</label>
-        <TextField
-          name="deptnombre"
-          value={formData.deptnombre}
+        <Select
+          name="dept_nombre"
+          value={formData.dept_nombre}
           onChange={handleChange}
           margin="normal"
           required
-        />
+        >
+          <MenuItem value="departamento1">Departamento 1</MenuItem>
+          <MenuItem value="departamento2">Departamento 2</MenuItem>
+          
+        </Select>
         <label>Personal Académico:</label>
-        <TextField
-          name="panombre"
-          value={formData.panombre}
+        <Select
+          name="pa_nombre"
+          value={formData.pa_nombre}
           onChange={handleChange}
           margin="normal"
           required
-        />
+        >
+          <MenuItem value="academico1">Académico 1</MenuItem>
+          <MenuItem value="academico2">Académico 2</MenuItem>
+          
+        </Select>
         <label>Actividad:</label>
-        <TextField
-          name="actnombre"
-          value={formData.actnombre}
+        <Select
+          name="act_nombre"
+          value={formData.act_nombre}
           onChange={handleChange}
           margin="normal"
           required
-        />
+        >
+          <MenuItem value="actividad1">Actividad 1</MenuItem>
+          <MenuItem value="actividad2">Actividad 2</MenuItem>
+          
+        </Select>
         <label>Oferta Vacantes:</label>
-        <TextField
-          name="ovnombre"
-          value={formData.ovnombre}
+        <Select
+          name="ofe_vacantes"
+          value={formData.ofe_vacantes}
           onChange={handleChange}
           margin="normal"
           required
-        />
+        >
+          <MenuItem value="vacantes1">Vacantes 1</MenuItem>
+          <MenuItem value="vacantes2">Vacantes 2</MenuItem>
+          
+        </Select>
         <label>Oferta Horas:</label>
-        <TextField
-          name="ohnombre"
-          value={formData.ohnombre}
+        <Select
+          name="ofe_horas"
+          value={formData.ofe_horas}
           onChange={handleChange}
           margin="normal"
           required
-        />
+        >
+          <MenuItem value="horas1">Horas 1</MenuItem>
+          <MenuItem value="horas2">Horas 2</MenuItem>
+          
+        </Select>
         <Button
           type="submit"
           variant="contained"
